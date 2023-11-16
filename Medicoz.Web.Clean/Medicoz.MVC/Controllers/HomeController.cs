1b164d5303f9cad8608da0e4ec80eadf1f0e0386
jekaH1 <ceyhun100203@gmail.com>
2023-11-16 18:13:43 +0400

    another billion changes on localservice

--- Medicoz.Web.Clean/Medicoz.MVC/Controllers/HomeController.cs
+++ Medicoz.Web.Clean/Medicoz.MVC/Controllers/HomeController.cs
@@ -14,24 +14,30 @@ namespace Medicoz.MVC.Controllers
         private readonly IUserService _userService;
         private readonly IDatabaseLocalisationRepository<Slider> _databaseLocalisationRepository;
         private readonly ILocalizationService<TestModel> _localizationService;
+        private readonly ITestRepository _testRepository;
 
         public HomeController(IUserService userService,
             IDatabaseLocalisationRepository<Slider> databaseLocalisationRepository,
-            ILocalizationService<TestModel> localizationService )
+            ILocalizationService<TestModel> localizationService
+          , ITestRepository testRepository)
         {
             _userService = userService;
             _databaseLocalisationRepository = databaseLocalisationRepository;
             _localizationService = localizationService;
+            _testRepository = testRepository;
         }
         public async Task<IActionResult> Index()
         {
-            var sliders = await _databaseLocalisationRepository.GetLocalizedEntities();
-            var test = _localizationService.GetAllEntitiesLocalizedValues("ru");
-
-
             HomeViewModel homeViewModel = new HomeViewModel();
+            var sliders = await _databaseLocalisationRepository.GetLocalizedEntities();
             homeViewModel.Sliders = sliders;
-            homeViewModel.EntitiesLocalizedValues = test;
+
+            homeViewModel.TestModels = await _testRepository.GetAllAsync();
+            var testModelTitle = _localizationService.GetAllEntitiesLocalizedValues(nameof(TestModel.Title), "ru");
+            var testModelDesc = _localizationService.GetAllEntitiesLocalizedValues(nameof(TestModel.Description), "ru");
+            homeViewModel.EntitiesLocalizedValuesTitle = testModelTitle;
+            homeViewModel.EntitiesLocalizedValuesDesc = testModelDesc;
+
             return View(homeViewModel);
         }
         public async Task<IActionResult> Create()
--- Medicoz.Web.Clean/Medicoz.MVC/ViewModels/HomeViewModel.cs
+++ Medicoz.Web.Clean/Medicoz.MVC/ViewModels/HomeViewModel.cs
@@ -7,7 +7,9 @@ namespace Medicoz.MVC.ViewModels
     {
         public User User { get; set; }
         public List<Slider> Sliders { get; set; }
-        public Dictionary<int,string> EntitiesLocalizedValues { get; set; }
+        public Dictionary<int,string> EntitiesLocalizedValuesTitle { get; set; }
+        public Dictionary<int,string> EntitiesLocalizedValuesDesc { get; set; }
+        public List<TestModel> TestModels { get; set; }
 
     }
 }
