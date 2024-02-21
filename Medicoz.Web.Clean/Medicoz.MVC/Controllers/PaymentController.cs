bae8f1ff50c1aad11737f1c0c9167438f981d64c
jekaH1 <ceyhun100203@gmail.com>
2024-02-21 23:58:52 +0400

    stripe by good boy

--- Medicoz.Web.Clean/Medicoz.MVC/Controllers/PaymentController.cs
+++ Medicoz.Web.Clean/Medicoz.MVC/Controllers/PaymentController.cs
@@ -1,6 +1,8 @@
 ﻿using Medicoz.Application.Contracts.Payment;
 using Microsoft.AspNetCore.Mvc;
 using Stripe;
+using Stripe.BillingPortal;
+using Stripe.Checkout;
 
 namespace Medicoz.MVC.Controllers
 {
@@ -9,7 +11,7 @@ namespace Medicoz.MVC.Controllers
         private readonly IConfiguration _configuration;
         private readonly IPaymentService _paymentService;
 
-        public PaymentController(IConfiguration configuration,IPaymentService paymentService)
+        public PaymentController(IConfiguration configuration, IPaymentService paymentService)
         {
             _configuration = configuration;
             _paymentService = paymentService;
@@ -19,10 +21,40 @@ namespace Medicoz.MVC.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Charge(string stripeEmail, string stripeToken, long amount)
+        public async Task<ActionResult> CreateCheckoutSession()
         {
-            _paymentService.Charge("ceyhun100203@gmail.com", 2000);
-            return RedirectToAction("Index","home");
+            StripeConfiguration.ApiKey = _configuration.GetSection("Stripe")["SecretKey"];
+            var domain = "https://localhost:7000";
+
+            var options = new Stripe.Checkout.SessionCreateOptions
+            {
+                SuccessUrl = "https://example.com/success",
+                LineItems = new List<Stripe.Checkout.SessionLineItemOptions>
+                {
+                  new Stripe.Checkout.SessionLineItemOptions
+                    {
+                      PriceData = new SessionLineItemPriceDataOptions
+                       {
+                            UnitAmount = 1233,
+                            Currency = "usd",
+                            ProductData = new SessionLineItemPriceDataProductDataOptions
+                            {
+                               Name = "Salamaleykuma",
+                            }
+                       },
+                      Quantity=1
+                     },
+                  
+                },
+                Mode = "payment",
+            };
+
+
+            var service = new Stripe.Checkout.SessionService();
+            Stripe.Checkout.Session session = service.Create(options);
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
         }
 
         public IActionResult PaymentSuccess()
--- Medicoz.Web.Clean/Medicoz.MVC/obj/Debug/net6.0/Medicoz.MVC.AssemblyInfo.cs
+++ Medicoz.Web.Clean/Medicoz.MVC/obj/Debug/net6.0/Medicoz.MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Medicoz.MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c4320e633e535427fa507d25161fc7f770c5dde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+063f177bda6eee9663fc6263f28e5dd0359c92f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Medicoz.MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Medicoz.MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
